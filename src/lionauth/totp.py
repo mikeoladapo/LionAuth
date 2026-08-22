aec4009598f17f665261d7fa19b16be4e65f709b
Micheal O. Oladokun <134233611+mikeoladapo@users.noreply.github.com>
2026-08-22 16:14:43 +0100

    Update totp.py

--- src/lionauth/totp.py
+++ src/lionauth/totp.py
@@ -9,6 +9,14 @@ class TOTPAuthenticator:
     def generate_secret(self) -> str:
         return pyotp.random_base32()
 
+    def _create_totp(self, secret: str) -> pyotp.TOTP:
+        return pyotp.TOTP(
+            secret,
+            digits=self.config.digits,
+            interval=self.config.interval,
+            digest=self.config.algorithm.lower(),
+        )
+
     def generate_otp(self, secret: str) -> str:
         totp = pyotp.TOTP(secret)
         return totp.now()
