9bc26434adbd723de82c8ee3639b6483541e9275
Micheal O. Oladokun <134233611+mikeoladapo@users.noreply.github.com>
2026-08-22 16:00:41 +0100

    Update totp.py

--- src/lionauth/totp.py
+++ src/lionauth/totp.py
@@ -3,3 +3,12 @@ import pyotp
 class TOTPAuthenticator:
     def generate_secret(self) -> str:
         return pyotp.random_base32()
+
+    def generate_otp(self, secret: str) -> str:
+        totp = pyotp.TOTP(secret)
+        return totp.now()
+
+    def verify(self, secret: str, otp: str) -> bool:
+        totp = pyotp.TOTP(secret)
+        return totp.verify(otp)
+    
